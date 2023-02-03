3cc091f6373825e4fe60aecccefb92536c6ee252
Mohammad Gupron <ghuvrons@gmail.com>
2023-02-03 09:04:03 +0700

    fixing bug

--- src/buffer.c
+++ src/buffer.c
@@ -24,19 +24,26 @@ int32_t Buffer_Read(Buffer_t *buf, void *dst, uint16_t itemNum)
   uint32_t readLen = 0;
   uint32_t length = (uint32_t)itemNum * (uint32_t)buf->itemSz;
   uint8_t *dstBytes = dst;
+  size_t i;
 
   if (buf == NULL || buf->buffer == NULL || dstBytes == NULL) return -1;
 
   while (length--) {
-    if (buf->r_idx == buf->w_idx && !buf->isOverlap) break;
-    *dstBytes = *(((uint8_t*)buf->buffer)+buf->r_idx);
+    for (i = 0; i < buf->itemSz; i++) {
+      if (buf->r_idx == buf->w_idx && !buf->isOverlap) {
+        goto finish;
+      }
+      *dstBytes = *(((uint8_t*)buf->buffer)+buf->r_idx);
+
+      dstBytes++;
+      buf->r_idx++;
+      if (buf->r_idx == buf->bufferLength) buf->r_idx = 0;
+    }
     buf->isOverlap = 0;
-    buf->r_idx++;
-    if (buf->r_idx == buf->bufferLength) buf->r_idx = 0;
-    dst++;
     readLen++;
   }
 
+finish:
   return readLen;
 }
 
@@ -44,22 +51,31 @@ int32_t Buffer_Read(Buffer_t *buf, void *dst, uint16_t itemNum)
 int32_t Buffer_Write(Buffer_t *buf, void *src, uint16_t itemNum)
 {
   uint16_t writeLen = 0;
-  uint32_t length = (uint32_t)itemNum * (uint32_t)buf->itemSz;
+  uint32_t length = (uint32_t)itemNum;
   uint8_t *srcBytes = src;
+  size_t i;
 
   if (buf == NULL || buf->buffer == NULL || srcBytes == NULL) return -1;
 
   while (length--) {
-    *(((uint8_t*)buf->buffer)+buf->w_idx) = *srcBytes;
+    for (i = 0; i < buf->itemSz; i++) {
+      *(((uint8_t*)buf->buffer)+buf->w_idx) = *srcBytes;
+
+      srcBytes++;
+      buf->w_idx++;
+      if (buf->w_idx == buf->bufferLength)  buf->w_idx = 0;
+      if (buf->w_idx == buf->r_idx)         buf->isOverlap = 1;
+    }
+    if (buf->isOverlap && buf->w_idx != buf->r_idx) {
+      // shift r_idx for one item size (skip one item)
+      for (i = 0; i < buf->itemSz; i++) {
+        buf->r_idx++;
+        if (buf->r_idx == buf->bufferLength) buf->r_idx = 0;
+      }
+      if (buf->w_idx != buf->r_idx) buf->isOverlap = 0;
+    }
 
     writeLen++;
-    srcBytes++;
-    buf->w_idx++;
-    if (buf->w_idx == buf->bufferLength) buf->w_idx = 0;
-    if (buf->isOverlap == 1) buf->r_idx = buf->w_idx;
-    if (buf->w_idx == buf->r_idx) {
-      buf->isOverlap = 1;
-    }
   }
 
   return writeLen;
